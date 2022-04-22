62f328dfa33952e2e2ae1a4e49c24478bfe1787f
Miroslaw Karwowski <mirek5207@gmail.com>
2022-04-22 11:28:21 +0200

    update sql script

--- src/main/resources/db.migration/V1_init.sql
+++ src/main/resources/db.migration/V1_init.sql
@@ -1,41 +1,32 @@
-CREATE TABLE Customer(
+CREATE TABLE Customer
+(
     customerId int not null primary key,
     name varchar(255) NOT NULL,
     surname varchar(255) NOT NULL,
     phoneNumber varchar(11) NOT NULL
 )
 
-CREATE TABLE Order {
-    orderId int not null primary key,
-    comment varchar(255) not null,
-    customerId int not null,
-    city varchar(255) not null,
-    street varchar(255) not null,
+CREATE TABLE Order
+(
+    orderId     int          not null primary key,
+    comment     varchar(255) not null,
+    customerId  int          not null,
+    city        varchar(255) not null,
+    street      varchar(255) not null,
     houseNumber varchar(255) not null,
-    FOREIGN KEY(customerId) REFERENCES Customer(CustomerId)
-}
-CREATE TABLE OrderItem{
-    orderItemId int not null primary key,
-    quantity int not null,
+    FOREIGN KEY (customerId) REFERENCES Customer (CustomerId)
+)
+CREATE TABLE OrderItem
+(
     orderId int not null,
     pizzaId int not null,
     FOREIGN KEY(orderId) REFERENCES Order(orderId),
     FOREIGN KEY(pizzaId) REFERENCES Order(pizzaId)
 
-}
-Create Table Pizza{
-    pizzaId int not null primary key,
-    ingredientId int not null,
-    pizzaSizeId int not null,
-    FOREIGN KEY(ingredientId) REFERENCES Ingredient(ingredientId)
-    FOREIGN Key(pizzaSizeId) REFERENCES pizzaSizeId(pizzaSizeId)
-}
-CREATE table Ingredient{
-    ingredientId int not null primary key,
-    name varchar(255) not null,
-    quantity int not null
-    }
-Create TABLE PizzaSize{
-    pizzaSizeId int not null primary key,
-    size int not null
-}
+)
+Create Table Pizza
+(
+    pizzaId int          not null primary key,
+    name    varchar(255) not null,
+    price   double       not null
+)
