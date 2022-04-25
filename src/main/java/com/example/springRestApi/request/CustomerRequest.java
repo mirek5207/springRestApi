6333174d32dc972e0d80c2403c10b3f6aba2b364
Miroslaw Karwowski <mirek5207@gmail.com>
2022-04-25 18:02:37 +0200

    update customer request

--- src/main/java/com/example/springRestApi/request/CustomerRequest.java
+++ src/main/java/com/example/springRestApi/request/CustomerRequest.java
@@ -7,7 +7,7 @@ import lombok.extern.jackson.Jacksonized;
 @Jacksonized
 @Value
 public class CustomerRequest {
-    private String login;
-    private String password;
-
+    private String name;
+    private String surname;
+    private String phoneNumber;
 }
