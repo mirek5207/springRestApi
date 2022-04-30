f4d3d409d0ab05c27be4f74a85f94da324843172
Miroslaw Karwowski <mirek5207@gmail.com>
2022-04-30 17:28:21 +0200

    fix: relationship of entities

--- src/main/java/com/example/springRestApi/entity/Pizza.java
+++ src/main/java/com/example/springRestApi/entity/Pizza.java
@@ -1,23 +1,32 @@
 package com.example.springRestApi.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Pizza {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "pizzaId",nullable = false)
+    @Column(name = "pizza_Id",nullable = false)
     private Long id;
 
     @Column(name = "name",nullable = false)
     private String name;
 
-    @Column(name = "city",nullable = false)
+    @Column(name = "price",nullable = false)
     private String price;
 
     @ManyToMany(mappedBy = "pizza")
-    private Set<Order> orders = new HashSet<>();
+    @JsonIgnore
+    private Set<Orders> orders = new HashSet<>();
 
 }
